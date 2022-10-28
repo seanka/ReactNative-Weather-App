12dd9f7a749a78df0a98b34eb843aa3d01d87897
Sean Kristian Anderson <seankristian16@gmail.com>
2022-10-28 22:56:38 +0700

    added component and styling to weather daily

--- src/components/bottom-container/WeatherDaily.js
+++ src/components/bottom-container/WeatherDaily.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { convertDate } from '../../libs/dateConverter';
 import { dailyWeather } from '../../libs/jsonParser';
@@ -15,6 +15,22 @@ function WeatherDaily({ weatherData }) {
           return (
             <View key={dayData.dt} style={styles.rootContainer}>
               <Text style={styles.dateText}>{date}</Text>
+              <View style={styles.rowContainer}>
+                <Image
+                  style={styles.weatherIcon}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${dayData.weather[0].icon}@4x.png`,
+                  }}
+                />
+                <View>
+                  <Text style={styles.tempText}>
+                    {Math.floor(dayData.temp.day)}°{' '}
+                  </Text>
+                  <Text style={styles.descText}>
+                    {dayData.weather[0].description}{' '}
+                  </Text>
+                </View>
+              </View>
             </View>
           );
         })}
@@ -27,11 +43,35 @@ const styles = StyleSheet.create({
   rootContainer: {
     marginHorizontal: 5,
     backgroundColor: '#e9e9e9',
-    borderRadius: 5,
+    borderRadius: 10,
     padding: 10,
     width: 200,
+    paddingHorizontal: 20,
   },
   dateText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  descriptionContainer: {
+    flexDirection: 'row',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  weatherIcon: {
+    height: 55,
+    width: 55,
+    marginRight: 5,
+  },
+  tempText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 14,
+  },
+  descText: {
     fontFamily: 'Poppins-Regular',
     fontSize: 12,
   },
